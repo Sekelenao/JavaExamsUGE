33805c706bed1de6eba90f15627b911a42c1af09
Sekelenao <Sekelenao@protonmail.com>
2024-07-26 21:00:08 +0200

    New version

--- 2023_2024/Expando/src/fr/uge/expando/Expando.java
+++ 2023_2024/Expando/src/fr/uge/expando/Expando.java
@@ -3,45 +3,153 @@ package fr.uge.expando;
 import java.lang.reflect.RecordComponent;
 import java.util.*;
 import java.util.function.BiConsumer;
+import java.util.function.Consumer;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
 public interface Expando {
 
     Map<String, Object> moreAttributes();
 
     default Map<String, Object> asMap() {
-        final var type = this;
+        final var instance = this;
         return new AbstractMap<>() {
 
-            private final Map<String, RecordComponent> fields = ExpandoUtils.CACHE.get(type.getClass());
+            private final Map<String, RecordComponent> components = ExpandoUtils.RECORD_COMPONENTS_CACHE
+                    .get(instance.getClass());
+
+            private final int size = moreAttributes().size() + components.size();
 
             @Override
             public Set<Entry<String, Object>> entrySet() {
-                return new ExpandoEntrySet(fields, moreAttributes(), type);
+                return new AbstractSet<>() {
+
+                    @Override
+                    public Iterator<Entry<String, Object>> iterator() {
+                        return new Iterator<>() {
+
+                            private final Iterator<Entry<String, RecordComponent>> componentsIt = components.entrySet()
+                                    .iterator();
+
+                            private final Iterator<Map.Entry<String, Object>> moreAttrIt = moreAttributes().entrySet()
+                                    .iterator();
+
+                            @Override
+                            public boolean hasNext() {
+                                return componentsIt.hasNext() || moreAttrIt.hasNext();
+                            }
+
+                            @Override
+                            public Entry<String, Object> next() {
+                                if (componentsIt.hasNext()) {
+                                    var entry = componentsIt.next();
+                                    var value = ExpandoUtils.invoke(entry.getValue().getAccessor(), instance);
+                                    return Map.entry(entry.getKey(), value);
+                                }
+                                if (moreAttrIt.hasNext()) return moreAttrIt.next();
+                                throw new NoSuchElementException();
+                            }
+                        };
+                    }
+
+                    @Override
+                    public int size() {
+                        return size;
+                    }
+
+                    @Override
+                    public Spliterator<Entry<String, Object>> spliterator() {
+                        return new Spliterator<>() {
+
+                            private final Iterator<Entry<String, RecordComponent>> componentsIt = components.entrySet()
+                                    .iterator();
+
+                            private final int componentsSize = components.size();
+
+                            private int componentsIndex = 0;
+
+                            private final Iterator<Map.Entry<String, Object>> moreAttrIt = moreAttributes().entrySet()
+                                    .iterator();
+
+                            private final int attributesSize = moreAttributes().size();
+
+                            private int attributesIndex = 0;
+
+                            boolean isSplit = false;
+
+                            @Override
+                            public boolean tryAdvance(Consumer<? super Entry<String, Object>> action) {
+                                if(componentsIt.hasNext()) {
+                                    var entry = componentsIt.next();
+                                    componentsIndex++;
+                                    var value = ExpandoUtils.invoke(entry.getValue().getAccessor(), instance);
+                                    action.accept(Map.entry(entry.getKey(), value));
+                                    return true;
+                                }
+                                if(!isSplit && moreAttrIt.hasNext()) {
+                                    action.accept(moreAttrIt.next());
+                                    attributesIndex++;
+                                    return true;
+                                }
+                                return false;
+                            }
+
+                            @Override
+                            public Spliterator<Entry<String, Object>> trySplit() {
+
+                                if(isSplit) return null;
+                                isSplit = true;
+                                return moreAttributes().entrySet().spliterator();
+                            }
+
+                            @Override
+                            public long estimateSize() {
+                                if (isSplit) return componentsSize - componentsIndex;
+                                return (componentsSize + attributesSize) - (attributesIndex + componentsIndex);
+                            }
+
+                            @Override
+                            public int characteristics() {
+                                return SIZED | SUBSIZED | IMMUTABLE | DISTINCT | NONNULL | ORDERED;
+                            }
+                        };
+                    }
+
+                    @Override
+                    public Stream<Entry<String, Object>> stream() {
+                        return StreamSupport.stream(spliterator(), false);
+                    }
+                };
             }
 
             @Override
             public boolean containsKey(Object key) {
-                return fields.containsKey(key) || moreAttributes().containsKey(key);
-            }
-
-            @Override
-            public Object get(Object key) {
-                var recordComponent = fields.get(key);
-                return recordComponent == null ? moreAttributes().get(key) :
-                        ExpandoUtils.invoke(recordComponent.getAccessor(), type);
+                return components.containsKey(key) || moreAttributes().containsKey(key);
             }
 
             @Override
             public Object getOrDefault(Object key, Object defaultValue) {
-                return containsKey(key) ? get(key) : defaultValue;
+                Objects.requireNonNull(key);
+                var component = components.get(key);
+                if (component != null) {
+                    return ExpandoUtils.invoke(component.getAccessor(), instance);
+                }
+                return moreAttributes().getOrDefault(key, defaultValue);
+            }
+
+            @Override
+            public Object get(Object key) {
+                Objects.requireNonNull(key);
+                return getOrDefault(key, null);
             }
 
             @Override
             public void forEach(BiConsumer<? super String, ? super Object> action) {
                 Objects.requireNonNull(action);
-                for (var e : entrySet()) action.accept(e.getKey(), e.getValue());
+                for(var entry : entrySet()) {
+                    action.accept(entry.getKey(), entry.getValue());
+                }
             }
-
         };
     }
 
--- 2023_2024/Expando/src/fr/uge/expando/ExpandoUtils.java
+++ 2023_2024/Expando/src/fr/uge/expando/ExpandoUtils.java
@@ -8,34 +8,41 @@ import java.util.stream.Collectors;
 
 public final class ExpandoUtils {
 
-    static final ClassValue<Map<String, RecordComponent>> CACHE = new ClassValue<>() {
-        @Override
-        protected Map<String, RecordComponent> computeValue(Class<?> type) {
-            Objects.requireNonNull(type);
-            return Arrays.stream(type.getRecordComponents())
-                    .filter(r -> !r.getName().equals("moreAttributes"))
-                    .collect(Collectors.toMap(RecordComponent::getName, r -> r,
-                            (existingValueWithKey, valueSameKey) -> {throw new AssertionError();}, LinkedHashMap::new));
-            //LinkedHashMap for Q9
-        }
-    };
+    private static final String MORE_ATTR_NAME = "moreAttributes";
 
-    private ExpandoUtils(){
+    private ExpandoUtils() {
         throw new AssertionError("You cannot instantiate this class.");
     }
 
+    static final ClassValue<Map<String, RecordComponent>> RECORD_COMPONENTS_CACHE = new ClassValue<>() {
+
+        @Override
+        protected Map<String, RecordComponent> computeValue(Class<?> type) {
+            return Arrays.stream(type.getRecordComponents())
+                    .filter(recordComponent -> !recordComponent.getName().equals(MORE_ATTR_NAME))
+                    .collect(Collectors.toMap(
+                            RecordComponent::getName,
+                            recordComponent -> recordComponent,
+                            (_, _) -> {throw new AssertionError();},
+                            LinkedHashMap::new)
+                    );
+        }
+
+    };
+
     public static <R extends Record & Expando> Map<String, Object> copyAttributes(Map<String, Object> moreAttributes, Class<R> type){
-        Objects.requireNonNull(type);
         Objects.requireNonNull(moreAttributes);
-        for(var e : moreAttributes.entrySet()){
-            Objects.requireNonNull(e.getValue());
-            if(CACHE.get(type).containsKey(Objects.requireNonNull(e.getKey()))){
+        Objects.requireNonNull(type);
+        var components = RECORD_COMPONENTS_CACHE.get(type);
+        for(var entry : moreAttributes.entrySet()) {
+            Objects.requireNonNull(entry.getValue());
+            if(components.containsKey(entry.getKey())) {
                 throw new IllegalArgumentException("You cannot add attributes with the same name of a record component");
             }
         }
         return Map.copyOf(moreAttributes);
     }
-    
+
     static Object invoke(Method accessor, Object expando) {
         Objects.requireNonNull(accessor);
         Objects.requireNonNull(expando);
