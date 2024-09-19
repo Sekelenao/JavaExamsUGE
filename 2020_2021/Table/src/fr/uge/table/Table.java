6f1b4a2d94d7d3be82eeeeaa6b2c8c8981ac7804
Sekelenao <Sekelenao@protonmail.com>
2024-09-19 23:16:38 +0200

    Q2 Table

--- 2020_2021/Table/src/fr/uge/table/Table.java
+++ 2020_2021/Table/src/fr/uge/table/Table.java
@@ -1,10 +1,29 @@
 package fr.uge.table;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
+import java.util.function.Function;
 
 public final class Table<T> {
+
+    public final class Group<E> {
+
+        private final Map<E, List<Integer>> positions;
+
+        private Group(Comparator<E> comparator, Function<T, E> keySupplier) {
+            Objects.requireNonNull(comparator);
+            Objects.requireNonNull(keySupplier);
+            this.positions = new TreeMap<>(comparator);
+            for(var i = 0; i < elements.size(); i++) {
+                var key = keySupplier.apply(elements.get(i));
+                positions.merge(key, new ArrayList<>(i), (l1, l2) -> { l1.addAll(l2); return l1; });
+            }
+        }
+
+        public int keySize(){
+            return positions.size();
+        }
+
+    }
 
     private final List<T> elements;
 
@@ -15,6 +34,7 @@ public final class Table<T> {
         this.isDynamic = isDynamic;
     }
 
+    @SafeVarargs
     private static <T> List<T> arrayToList(T... elements) {
         var lst = new ArrayList<T>(elements.length);
         for (var element : elements) {
@@ -38,5 +58,10 @@ public final class Table<T> {
         return elements.size();
     }
 
+    public <E> Group<E> groupBy(Function<T, E> keySupplier, Comparator<E> comparator){
+        Objects.requireNonNull(comparator);
+        Objects.requireNonNull(keySupplier);
+        return new Group<>(comparator, keySupplier);
+    }
 
 }
