82be5412057835578d65ebceba216ed8ec623c9d
Sekelenao <Sekelenao@protonmail.com>
2024-09-25 00:16:55 +0200

    Q6 Configurations

--- 2018_2019/Configurations/src/fr/uge/configurations/ConfHelper.java
+++ 2018_2019/Configurations/src/fr/uge/configurations/ConfHelper.java
@@ -1,6 +1,7 @@
 package fr.uge.configurations;
 
 import java.util.Arrays;
+import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
 import java.util.function.Function;
@@ -13,7 +14,7 @@ public final class ConfHelper {
     }
 
     @SafeVarargs
-    public static <T> String toString(T instance, Function<T, Optional<?>>... getters){
+    public static <T> String toString(T instance, Function<? super T, Optional<?>>... getters){
         Objects.requireNonNull(instance);
         Objects.requireNonNull(getters);
         return Arrays.stream(getters)
@@ -21,6 +22,18 @@ public final class ConfHelper {
                 .filter(Optional::isPresent)
                 .map(Optional::get)
                 .map(Object::toString)
+                .collect(Collectors.joining(", ", "{", "}"));
+    }
+
+    @SafeVarargs
+    public static <T> String toExtendedString(T instance, Map.Entry<String, Function<? super T, Optional<?>>>... pairs){
+        Objects.requireNonNull(instance);
+        Objects.requireNonNull(pairs);
+        return Arrays.stream(pairs)
+                .map(pair -> Map.entry(pair.getKey(), pair.getValue().apply(instance)))
+                .filter(pair -> pair.getValue().isPresent())
+                .map(pair -> Map.entry(pair.getKey(), pair.getValue().get()))
+                .map(pair -> String.join(": ", pair.getKey(), pair.getValue().toString()))
                 .collect(Collectors.joining(", ", "{", "}"));
     }
 
