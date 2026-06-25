9a4f9b400ab82f868b0abadb19e9e858c04913b5
Sekelenao <Sekelenao@protonmail.com>
2026-06-25 23:50:04 +0200

    Q3

--- 2025_2026/IntSet/IntSet/src/fr/uge/intset/IntSet.java
+++ 2025_2026/IntSet/IntSet/src/fr/uge/intset/IntSet.java
@@ -1,12 +1,13 @@
 package fr.uge.intset;
 
+import java.util.Arrays;
 import java.util.Objects;
 
 public final class IntSet {
 
     private static final int DEFAULT_CAPACITY = 4;
 
-    private final int[] bitset;
+    private int[] bitset;
 
     public IntSet() {
         this.bitset = new int[DEFAULT_CAPACITY];
@@ -23,11 +24,18 @@ public final class IntSet {
         bitset[index >> 5] |= 1 << (index & 31);
     }
 
+    private void growIfNecessary(int bitsetIndex){
+        if (bitsetIndex >= bitset.length){
+            bitset = Arrays.copyOf(bitset, Math.max(bitset.length * 2, bitsetIndex + 1));
+        }
+    }
+
     public boolean add(int value){
         if(value < 0) {
             throw new IllegalArgumentException("Value must be positive");
         }
         var bitsetIndex = value >> 5;
+        growIfNecessary(bitsetIndex);
         var targetBitMask = 1 << (value & 31);
         if((bitset[bitsetIndex] & targetBitMask) != 0){
             return false;
